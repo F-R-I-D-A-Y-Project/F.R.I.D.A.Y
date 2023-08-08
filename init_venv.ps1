c6097ba732ef0c2626afbb0dd821d2523cbdeca8
varad-comrad <fabricio_asfora_002@hotmail.com>
2023-08-08 10:46:40 -0300

    included PS1 script

---
+++ init_venv.ps1
@@ -0,0 +1,11 @@
+function InitVenv {
+
+    # Create a Python virtual environment
+    python -m venv env
+
+    # Activate the virtual environment
+    & "env\Scripts\Activate.ps1"
+
+    # Install requirements using pip
+    pip install -r requirements.txt
+}
